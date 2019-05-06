7f0e35d5fef57fbbddea887f7d1928146a41d8c2
Simon Mitchell <kartano@gmail.com>
2019-05-05 20:27:33 -0700

    Added Font Awesome 5.8.1

--- SunsetCoders/PageRender/PageRender.php
+++ SunsetCoders/PageRender/PageRender.php
@@ -90,6 +90,7 @@ abstract class PageRender
                 $this->renderDocumentReadyJS();
                 $this->renderJS();
                 ?>
+	            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
             </HEAD>
             <BODY>
                 <DIV class="container-fluid">
